75e79b085a8e00b47720348ceba7f17b45f39e39
Daku-ignitus <70779685+Daku-ignitus@users.noreply.github.com>
2020-09-07 17:20:10 -0500

    sprite changes

--- main.ts
+++ main.ts
@@ -65,41 +65,41 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     while (controller.left.isPressed()) {
         mySprite.setImage(img`
-            . . f f f f . . . . . . . . . . 
-            . . f . . f f . . . . . . . . . 
-            . . f . . . f . . . . . . . . . 
-            . . f f . f . . . . . . . . . . 
-            . . . . f f f f f f f f . . . . 
-            . . f f f f . . . . . f . . . . 
-            . . f . . f f f . . . . . . . . 
-            . . f . . . . f f f . . . . . . 
-            . . f . . . . . . f f f . . . . 
-            . . . . . . . f f . . f f . . . 
-            . . . . . f f . . . . . . f . . 
-            . . . f f . . . . . . . . f f . 
-            . . f . . . . . . . . . . f f . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . . . f f f f f f . . . . . . 
+            . . . f f f f f f f f . . . . . 
+            . . f f f f f f f f f f f . . . 
+            . f f f f f f f f f f f f . . . 
+            . f f f f f f f f f f f f . . . 
+            . f f f e e e f f f f f f f . . 
+            . f e e 4 4 8 b e 4 4 e f f . . 
+            . . f e d d 8 1 4 d 4 e e f . . 
+            . . . f d d d d 4 e e e f . . . 
+            . . . f e 4 4 4 e e f f . . . . 
+            . . . f 6 6 6 e d d 4 . . . . . 
+            . . . f 6 6 6 e d d e . . . . . 
+            . . . f 9 9 9 f e e f . . . . . 
+            . . . . f f f f f f . . . . . . 
+            . . . . . . f f f . . . . . . . 
             `)
         pause(100)
         mySprite.setImage(img`
-            . . . . . . . . . f . . . . . . 
-            . . . . . . . . f . f . . . . . 
-            . . . . . . . . f . f . . . . . 
-            . . . . . . . . . f . . . . . . 
-            . . . . . f f f f f f f f f . . 
-            . . . . f . . f f f f . . f f . 
-            . . . . f . . . f f . . . . f . 
-            . . . . f . . . f f . . . . . . 
-            . . . . . . . . f f f . . . . . 
-            . . . . . . . f f . f f . . . . 
-            . . . . . . f f . . . f . . . . 
-            . . . . . f . . . . . f . . . . 
-            . . . . f . . . . . . f . . . . 
-            . . . . . . . . . . f . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . . . f f f f f f . . . . . . 
+            . . . f f f f f f f f f . . . . 
+            . . f f f f f f f f f f f . . . 
+            . . f f f f f f f f f f f . . . 
+            . f f f f f f f f f f f f . . . 
+            . f f f f f f f f f f f f . . . 
+            . f f f e e e f f f f f f f . . 
+            . f e e 4 4 8 b e 4 4 e f f . . 
+            . . f e d d 8 1 4 d 4 e e f . . 
+            . . . f d d d e e e e e f . . . 
+            . . . f e 4 e d d 4 f . . . . . 
+            . . . f 6 6 e d d e f . . . . . 
+            . . f f 9 9 f e e f f f . . . . 
+            . . f f f f f f f f f f . . . . 
+            . . . f f f . . . f f . . . . . 
             `)
         pause(100)
     }
@@ -153,41 +153,41 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     while (controller.down.isPressed()) {
         mySprite.setImage(img`
-            . . f f f f . . . . . . . . . . 
-            . . f . . f f . . . . . . . . . 
-            . . f . . . f . . . . . . . . . 
-            . . f f . f . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
             . . . . f f f f f f f f . . . . 
-            . . f f f f . . . . . f . . . . 
-            . . f . . f f f . . . . . . . . 
-            . . f . . . . f f f . . . . . . 
-            . . f . . . . . . f f f . . . . 
-            . . . . . . . f f . . f f . . . 
-            . . . . . f f . . . . . . f . . 
-            . . . f f . . . . . . . . f f . 
-            . . f . . . . . . . . . . f f . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . . f f f f f f f f f f . . . 
+            . . . f f f f f f f f f f . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f f f f f f f f f f f f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b 8 4 4 8 b f e f f . 
+            . f f e 4 1 8 d d 8 1 4 e f f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 6 6 6 6 6 6 f 4 e . . 
+            . . 4 d f 6 6 6 6 6 6 f d 4 . . 
+            . . 4 4 f 9 9 9 9 9 9 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
             `)
         pause(100)
         mySprite.setImage(img`
-            . . . . . . . . . f . . . . . . 
-            . . . . . . . . f . f . . . . . 
-            . . . . . . . . f . f . . . . . 
-            . . . . . . . . . f . . . . . . 
-            . . . . . f f f f f f f f f . . 
-            . . . . f . . f f f f . . f f . 
-            . . . . f . . . f f . . . . f . 
-            . . . . f . . . f f . . . . . . 
-            . . . . . . . . f f f . . . . . 
-            . . . . . . . f f . f f . . . . 
-            . . . . . . f f . . . f . . . . 
-            . . . . . f . . . . . f . . . . 
-            . . . . f . . . . . . f . . . . 
-            . . . . . . . . . . f . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f f f f f f . . . . 
+            . . . f f f f f f f f f f . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f f f f f f f f f f f f . . 
+            . f f f f f f f f f f f f f f . 
+            . f f f f f e e e e f f f f f . 
+            . . f e f b 8 4 4 8 b f e f . . 
+            . . f e 4 1 8 d d 8 1 4 e f . . 
+            . . e f e 4 d d d d 4 e f . . . 
+            . . e 4 d d e 6 6 6 6 f e f . . 
+            . . . e d d e 6 6 6 6 f 4 e . . 
+            . . . . e e f 9 9 9 9 f . . . . 
+            . . . . . f f f f f f f . . . . 
+            . . . . . . . . . f f f . . . . 
             `)
         pause(100)
     }
