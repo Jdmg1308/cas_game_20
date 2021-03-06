01b11cb91f446ee88c2382bd6fb17a1d67e27f69
Daku-ignitus <70779685+Daku-ignitus@users.noreply.github.com>
2021-03-05 19:10:04 -0500

    locker

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image45":
-            case "myImage":return img`
-. . . c c c c c c c . . . . . . 
-. . c b 2 2 2 2 b c c . . . . . 
-. f b b b 2 b b b c c c . . . . 
-. f c c b b b c c b c c c . . . 
-. f 4 e c b c e 4 b c b c . . . 
-. f b b b b b b d d b b f . . . 
-. f d b d b d d d b b b f . . . 
-. . f d 2 2 2 d b b b b f c . . 
-. . . f 2 2 f c b f b c a a c . 
-. . c 2 2 b b f f b b c b b a c 
-. c a b b b f b b c c c b b c c 
-. c a b b f c c c c c b b c . . 
-. f a b b c c c c b b b b f . . 
-. c a a b b c b b b b b c f . . 
-. . c a a a a a a a a a f . . . 
-. . . c c c c c c c c f . . . . 
-`;
             case "image1":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -813,6 +794,25 @@ namespace myImages {
 . . 4 4 f 7 7 7 7 7 7 f 4 4 . . 
 . . . . . 8 8 8 8 8 8 . . . . . 
 . . . . . 8 8 . . 8 8 . . . . . 
+`;
+            case "image45":
+            case "myImage":return img`
+. . . c c c c c c c . . . . . . 
+. . c b 2 2 2 2 b c c . . . . . 
+. f b b b 2 b b b c c c . . . . 
+. f c c b b b c c b c c c . . . 
+. f 4 e c b c e 4 b c b c . . . 
+. f b b b b b b d d b b f . . . 
+. f d b d b d d d b b b f . . . 
+. . f d 2 2 2 d b b b b f c . . 
+. . . f 2 2 f c b f b c a a c . 
+. . c 2 2 b b f f b b c b b a c 
+. c a b b b f b b c c c b b c c 
+. c a b b f c c c c c b b c . . 
+. f a b b c c c c b b b b f . . 
+. c a a b b c b b b b b c f . . 
+. . c a a a a a a a a a f . . . 
+. . . c c c c c c c c f . . . . 
 `;
         }
         return null;
